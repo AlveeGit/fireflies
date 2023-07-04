732900b68ecbf7dbbb013687519f4262915bf4c7
AlveeGit <alveekabir1@gmail.com>
2023-07-04 16:15:45 +0600

    auth page

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,18 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Input from "@/components/input";
 
 const Auth = () => {
-  const [email, setEmail] = useStatse("");
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [variant, setVariant] = useState("login");
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-cover bg-fixed bg-center">
       <div className="bg-black w-full h-full bg-opacity-50">
@@ -11,16 +21,54 @@ const Auth = () => {
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl font-semibold mb-8">Sign in</h2>
+            <h2 className="text-white text-4xl font-semibold mb-8">
+              {variant === "login" ? "Sign in" : "Register"}
+            </h2>
             <div className=" flex flex-col gap-4">
+              {variant === "register" && (
+                <Input
+                  id="name"
+                  type="text"
+                  label="Username"
+                  value={name}
+                  onChange={(e: any) => setName(e.target.value)}
+                />
+              )}
               <Input
-                label="Email"
-                onChange={() => {}}
                 id="email"
                 type="email"
-                value=""
+                label="Email address or phone number"
+                value={email}
+                onChange={(e: any) => setEmail(e.target.value)}
+              />
+              <Input
+                type="password"
+                id="password"
+                label="Password"
+                value={password}
+                onChange={(e: any) => setPassword(e.target.value)}
               />
             </div>
+
+            <button
+              // onClick={variant === "login" ? login : register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
+              {variant === "login" ? "Login" : "Sign up"}
+            </button>
+
+            <p className="text-neutral-500 mt-12">
+              {variant === "login"
+                ? "First time using Netflix?"
+                : "Already have an account?"}
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
+              >
+                {variant === "login" ? "Create an account" : "Login"}
+              </span>
+              .
+            </p>
           </div>
         </div>
       </div>
